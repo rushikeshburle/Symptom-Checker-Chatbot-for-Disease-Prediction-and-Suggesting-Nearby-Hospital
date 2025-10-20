8c9ba2abb7ccae66e696ecdc54271145de71aecb
Rushikesh Burle <143891803+rushikeshburle@users.noreply.github.com>
2025-10-20 14:47:18 +0530

    app.py

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ def predict():
     message = {"answer": response}
     return jsonify(message)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
 
